37230eba2c7f84a0534d800f929f4461c6336e6b
adeankush3 <adeankush3@gmail.com>
2022-02-25 12:30:45 +0530

    [Ankush]Create a Person Data Management Program

---
+++ PersonDataMangement/PersonDataMangement/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDataMangement
+{
+    public class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+
+        public Person(string SSN,string Name,String Address,int Age)
+        {
+            this.SSN = SSN;
+            this.Name = Name;
+            this.Address = Address;
+            this.Age = Age;
+        }
+        
+    }
+}
